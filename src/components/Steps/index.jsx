d78493f5bd03c2e89be1562764fe2dd6216d0265
unknown <hov2001val@gmail.com>
2024-02-24 02:58:24 +0400

    steps setup

---
+++ src/components/Steps/index.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import StepMessage from "../StepMassage";
+import Button from "../Button";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+const Steps = () => {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handlePrevious = () => {
+    step > 1 ? setStep((s) => s - 1) : setStep((s) => (s = 3));
+  };
+
+  const handleNext = () => {
+    step < 3 ? setStep((s) => s + 1) : setStep((s) => (s = 1));
+  };
+
+  return (
+    <div>
+      <button
+        className="close"
+        onClick={() =>
+          setIsOpen((is) => {
+            if (is) {
+              setIsOpen(!is);
+              setStep(1);
+            } else {
+              setIsOpen(!is);
+            }
+          })
+        }
+      >
+        &times;
+      </button>
+
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+
+          <StepMessage step={step}>
+            {messages[step - 1]}
+            <div className="buttons">
+              <Button
+                bgColor="#e7e7e7"
+                textColor="#333"
+                onClick={() => alert(`Learn how to ${messages[step - 1]}`)}
+              >
+                Learn how
+              </Button>
+            </div>
+          </StepMessage>
+
+          <div className="buttons">
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
+              <span>👈</span> Previous
+            </Button>
+
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
+              Next <span>👉</span>
+              <span>🤓</span>
+            </Button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Steps;
